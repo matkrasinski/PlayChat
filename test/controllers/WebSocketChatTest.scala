d72c9e389b39499ad000fac014d2f5526db51342
Jakub Czyżewski <100937354+czyzewskijakub@users.noreply.github.com>
2024-06-15 15:17:34 +0200

    Tests (#20)
    * add tests for websocket index
    
    * optimise imports
    
    * remove failing test
    
    ---------
    
    Co-authored-by: Jakub Czyżewski <jakub.czyzewski@tomtom.com>

---
+++ test/controllers/WebSocketChatTest.scala
@@ -0,0 +1,63 @@
+package controllers
+
+import models.UserChat
+import org.apache.pekko.actor.ActorSystem
+import org.joda.time.DateTime
+import org.mockito.Mockito.when
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.http.Status.{OK, SEE_OTHER}
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, redirectLocation, status, stubControllerComponents}
+import play.api.test.{FakeRequest, Injecting}
+import repositories.UserChatRepository
+
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.Future
+
+class WebSocketChatTest extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar with BeforeAndAfterEach {
+
+  private val testToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MzYwMTcxODQ1MDAyNiwiaWF0IjoxNzE4NDUzNjI2fQ.KngIMpDjd_Bh0rTKm62yscFbE8M6XHt3rGXmxayfr4M"
+
+  var userChatRepository: UserChatRepository = null
+  var chatHistory: Seq[UserChat] = null
+  var webSocketChat: WebSocketChat = null
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    userChatRepository = mock[UserChatRepository]
+    val userChat = mock[UserChat]
+    when(userChat.message).thenReturn("hello")
+    when(userChat.userName).thenReturn("testuser")
+    when(userChat.dateTime).thenReturn(Some(new DateTime()))
+    chatHistory = Seq(userChat)
+    webSocketChat = new WebSocketChat(stubControllerComponents())(ActorSystem(), userChatRepository, global)
+  }
+
+  "WebSocketChat GET" should {
+
+    "chats history is shown on join" in {
+      when(userChatRepository.findAll()).thenReturn(Future.successful(chatHistory))
+
+      val chatRequest = FakeRequest(GET, "/chat")
+        .withSession("jwtToken" -> testToken)
+      val result = webSocketChat.index()(chatRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) must include("testuser - hello")
+    }
+
+    "chats history is shown not shown due to invalid token" in {
+      when(userChatRepository.findAll()).thenReturn(Future.successful(chatHistory))
+
+      val chatRequest = FakeRequest(GET, "/chat")
+        .withSession("jwtToken" -> testToken.replace("a", "b"))
+      val result = webSocketChat.index()(chatRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some("/login")
+    }
+
+  }
+}
